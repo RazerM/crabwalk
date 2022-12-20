0ae04773b29185de6effc9915ac294865ce0f282
Frazer McLean <frazer@frazermclean.co.uk>
2022-12-20 15:09:26 +0000

    Export all errors at top level

--- src/crabwalk/__init__.py
+++ src/crabwalk/__init__.py
@@ -1,9 +1,21 @@
 from ._lib import DirEntry, Types, Walk
-from ._types import GlobError, IgnoreError, LoopError, Override, Overrides
+from ._types import (
+    GlobError,
+    IgnoreError,
+    LoopError,
+    Override,
+    Overrides,
+    PartialError,
+    UnrecognizedFileTypeError,
+)
 
 GlobError.__module__ = __name__
 IgnoreError.__module__ = __name__
 LoopError.__module__ = __name__
+Override.__module__ = __name__
+Overrides.__module__ = __name__
+PartialError.__module__ = __name__
+UnrecognizedFileTypeError.__module__ = __name__
 
 __all__ = (
     "DirEntry",
