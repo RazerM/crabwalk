5497a9bbb01cf1441df9996bb151cd9407438dba
Frazer McLean <frazer@frazermclean.co.uk>
2022-12-21 23:54:16 +0000

    Build abi3 wheels without having to pass --py-limited-api

--- setup.py
+++ setup.py
@@ -2,6 +2,21 @@ import platform
 
 from setuptools import setup
 from setuptools_rust import RustExtension
+from wheel.bdist_wheel import bdist_wheel
+
+ABI_VERSION = "37"
+
+
+class bdist_wheel_abi3(bdist_wheel):
+    def get_tag(self):
+        python, abi, plat = super().get_tag()
+
+        if python.startswith("cp"):
+            # on CPython, our wheels are abi3 and compatible back to ABI_VERSION
+            return f"cp{ABI_VERSION}", "abi3", plat
+
+        return python, abi, plat
+
 
 setup(
     rust_extensions=[
@@ -11,9 +26,12 @@ setup(
             py_limited_api=True,
             # Enable abi3 mode if we're not using PyPy.
             features=(
-                [] if platform.python_implementation() == "PyPy" else ["pyo3/abi3-py37"]
+                []
+                if platform.python_implementation() == "PyPy"
+                else [f"pyo3/abi3-py{ABI_VERSION}"]
             ),
             rust_version=">=1.48.0",
         )
-    ]
+    ],
+    cmdclass={"bdist_wheel": bdist_wheel_abi3},
 )
