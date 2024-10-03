4ec7dec3c4ac319bb73ebf67fd60c41c50e4143d
Frazer McLean <frazer@frazermclean.co.uk>
2024-10-03 04:19:58 +0200

    Add more tests

--- tests/test_walk.py
+++ tests/test_walk.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import sys
 from pathlib import Path
 from typing import Callable, Iterator, List
@@ -17,6 +18,16 @@ else:
 
 
 WalkPaths: TypeAlias = Callable[[Walk], Iterator[str]]
+WalkEntries: TypeAlias = Callable[[Walk], Iterator[DirEntry]]
+
+
+@pytest.fixture(name="walk_entries")
+def walk_entries_fixture() -> WalkEntries:
+    def walk_entries(walk: Walk) -> Iterator[DirEntry]:
+        with walk:
+            yield from walk
+
+    return walk_entries
 
 
 @pytest.fixture(name="walk_paths")
@@ -416,3 +427,78 @@ def test_filter_entry_exception(tree_path: Path) -> None:
         with Walk("root", filter_entry=filter_entry, sort=True) as walk:
             for _ in walk:
                 pass
+
+
+@pytest.mark.tree(
+    Directory(
+        "root",
+        File("foo"),
+    ),
+    chdir=True,
+)
+def test_direntry_fspath(tree_path: Path, walk_entries: WalkEntries) -> None:
+    root, foo = walk_entries(Walk("root", sort=True))
+    assert os.fspath(root) == root.path == "root"
+    assert os.fspath(foo) == foo.path == "root/foo"
+
+
+@pytest.mark.tree(
+    Directory(
+        "root",
+        File("foo"),
+    ),
+)
+def test_direntry_fspath_absolute(tree_path: Path, walk_entries: WalkEntries) -> None:
+    root_path = tree_path / "root"
+    root, foo = walk_entries(Walk(root_path, sort=True))
+    assert os.fspath(root) == root.path == str(root_path)
+    assert os.fspath(foo) == foo.path == str(root_path / "foo")
+
+
+@pytest.mark.tree(
+    Directory(
+        "root",
+        File("file1"),
+        Directory("dir1"),
+        Symlink("link1", "file1"),
+        Symlink("link2", "dir1"),
+    ),
+    chdir=True,
+)
+@pytest.mark.parametrize(
+    "follow_symlinks",
+    [True, False],
+    ids=lambda follow_symlinks: f"follow_symlinks={follow_symlinks}",
+)
+def test_direntry_against_os_direntry(tree_path: Path, follow_symlinks: bool) -> None:
+    with os.scandir("root") as it:
+        os_entries = sorted(it, key=lambda entry: entry.path)
+
+    assert [entry.path for entry in os_entries] == [
+        "root/dir1",
+        "root/file1",
+        "root/link1",
+        "root/link2",
+    ]
+
+    with Walk("root", follow_symlinks=follow_symlinks) as walk:
+        root = next(walk)
+        assert root.path == "root"
+        cw_entries = sorted(walk, key=lambda entry: entry.path)
+
+    for os_entry, cw_entry in zip(os_entries, cw_entries):
+        assert os.fspath(os_entry) == os.fspath(cw_entry)
+        assert os_entry.path == cw_entry.path
+        assert os_entry.name == cw_entry.name
+
+        # This difference is documented in the inode method documentation.
+        if follow_symlinks:
+            assert os_entry.inode() == os.stat(cw_entry, follow_symlinks=False).st_ino
+        else:
+            assert os_entry.inode() == cw_entry.inode()
+
+        assert os_entry.is_dir(follow_symlinks=follow_symlinks) == cw_entry.is_dir()
+        assert os_entry.is_file(follow_symlinks=follow_symlinks) == cw_entry.is_file()
+        assert os_entry.is_symlink() == cw_entry.is_symlink()
+        assert os_entry.stat(follow_symlinks=follow_symlinks) == cw_entry.stat()
+        assert cw_entry.follow_symlinks == follow_symlinks
